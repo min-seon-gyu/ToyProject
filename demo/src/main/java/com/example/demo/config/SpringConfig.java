81f2f8a54b2bee5da117bac530508a7179b0a74c
민선규 <gcael@naver.com>
2022-10-16 13:05:34 +0900

    1016

---
+++ demo/src/main/java/com/example/demo/config/SpringConfig.java
@@ -0,0 +1,35 @@
+package com.example.demo.config;
+
+import com.example.demo.repository.JdbcMemberRepository;
+import com.example.demo.repository.MemberRepository;
+import com.example.demo.repository.MemoryMemberRepository;
+import com.example.demo.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public MemberService memberService(){
+
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository(){
+
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
+    }
+}
--- demo/src/main/java/com/example/demo/controller/HelloController.java
+++ demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class HelloController {
 
@@ -29,23 +32,51 @@ public class HelloController {
 
     @GetMapping("hello-api")
     @ResponseBody
-    public Hello helloApi(@RequestParam("name") String name) {
+    public List<Hello> helloApi() {
+
+        List<Hello> aa = new ArrayList<>();
         Hello hello = new Hello();
-        hello.setName(name);
-        return hello;
+        hello.setName("민선규");
+        hello.setAge(27);
+        hello.setJob("개발자");
+        aa.add(hello);
+
+        Hello hello2 = new Hello();
+        hello2.setName("민선규");
+        hello2.setAge(27);
+        hello2.setJob("개발자");
+        aa.add(hello2);
+        return aa;
     }
 
 
     static class Hello {
         private String name;
+        private int age;
+        private String job;
 
-
-        public String getName() {
+            public String getName() {
             return name;
         }
 
         public void setName(String name) {
             this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public String getJob() {
+            return job;
+        }
+
+        public void setJob(String job) {
+            this.job = job;
         }
     }
 
