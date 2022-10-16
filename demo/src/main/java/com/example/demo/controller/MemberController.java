c7a8f8a4848bff679b9d9ca5200c8b1865ccc899
min-seon-gyu <gcael@naver.com>
2022-10-16 20:02:17 +0900

    221016

--- demo/src/main/java/com/example/demo/controller/MemberController.java
+++ demo/src/main/java/com/example/demo/controller/MemberController.java
@@ -5,8 +5,8 @@ import com.example.demo.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.yaml.snakeyaml.events.Event;
 
 import java.util.List;
 
@@ -42,6 +42,9 @@ public class MemberController {
     }
 
 
+
+
 }
+
 
 
