2ba9693e700a1e8f9cf4d26e1a77954f3cb51152
민선규 <gcael@naver.com>
2023-02-25 02:37:13 +0900

    20230224

--- WebProject/src/main/java/com/WebProject/config/RedisConfig.java
+++ WebProject/src/main/java/com/WebProject/config/RedisConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -18,7 +19,9 @@ public class RedisConfig {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
+        jedisConnectionFactory.setPort(port);
+        //return new LettuceConnectionFactory(host, port);
     }
 
     @Bean
